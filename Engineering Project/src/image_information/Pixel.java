e582a86796622e40f8bb194a8c793700bfedbc13
Selim <87266516+Enchanter73@users.noreply.github.com>
2021-07-11 16:06:58 +0300

    Add files via upload

---
+++ Engineering Project/src/image_information/Pixel.java
@@ -0,0 +1,13 @@
+package image_information;
+
+public class Pixel {
+	public int r;
+	public int g;
+	public int b;
+	
+	Pixel(int r, int g, int b) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+}
